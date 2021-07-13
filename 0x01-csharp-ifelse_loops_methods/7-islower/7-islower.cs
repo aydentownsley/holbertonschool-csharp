e472ecf89b2e1b93b88b6509d02ce5e0d78f5e7a
aydentownsley <aydentownsley@gmail.com>
2021-07-13 12:22:34 -0500

    adding problem 7

---
+++ 0x01-csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Character
+{
+    public static bool IsLower(char c)
+    {
+        if ((int)c > 64 && (int)c < 91)
+            return false;
+        else
+            return true;
+    }
+}
