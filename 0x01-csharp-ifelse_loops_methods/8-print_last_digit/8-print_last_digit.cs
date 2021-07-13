e751406a10a7edf527198805cfaedb347db1e99a
aydentownsley <aydentownsley@gmail.com>
2021-07-13 12:30:57 -0500

    adding problem 8

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        if (number < 0)
+            number = number * -1;
+        Console.Write("{0}", number % 10);
+        return number % 10;
+    }
+}
