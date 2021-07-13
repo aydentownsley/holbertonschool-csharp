2985e0ad5834a3e2bb9f29fdb1953bd8d9625669
aydentownsley <aydentownsley@gmail.com>
2021-07-13 11:51:33 -0500

    adding problem 4

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = 0; i < 99; i++)
+            {
+                Console.Write("{0} = 0x{1:x}\n", i, i);
+            }
+        }
+    }
+}
