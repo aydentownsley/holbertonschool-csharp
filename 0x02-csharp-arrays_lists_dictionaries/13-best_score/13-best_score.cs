f9cf6b24dd097ec37aa498b329cbe1ebc1b638e6
aydentownsley <aydentownsley@gmail.com>
2021-07-14 15:33:45 -0500

    adding problem 13

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        string name = "";
+        int score = myList.Values.Max();
+        foreach (var key in myList)
+        {
+            if (key.Value == score)
+                name = key.Key;
+        }
+        return name;
+    }
+}
