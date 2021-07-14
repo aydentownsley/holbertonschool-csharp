4d9edad5cbf27e80eae1855d81a0cbd1554356dc
aydentownsley <aydentownsley@gmail.com>
2021-07-14 12:10:35 -0500

    adding problem 3

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        if (array == null || array.Length == 0)
+        {
+            Console.WriteLine();
+        }
+        else
+        {
+            for (int x = array.Length - 1; x >= 0; x--)
+            {
+                if (x == 0)
+                    Console.WriteLine(array[x]);
+                else
+                    Console.Write(array[x] + " ");
+            }
+        }
+    }
+}
