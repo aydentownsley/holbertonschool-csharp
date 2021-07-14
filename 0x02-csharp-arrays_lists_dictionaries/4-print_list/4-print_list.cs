3f761eff3b0307bce1d131bc57a8a6bdcbaa6d64
aydentownsley <aydentownsley@gmail.com>
2021-07-14 12:41:12 -0500

    adding problem 4

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        List<int> newList = new List<int>();
+
+        if (size == 0)
+            Console.WriteLine();
+
+        for (int x = 0; x < size; x++)
+        {
+            newList.Add(x);
+            if (x == size - 1)
+                Console.WriteLine(newList[x]);
+            else
+                Console.Write(newList[x] + " ");
+        }
+
+        return newList;
+    }
+}
