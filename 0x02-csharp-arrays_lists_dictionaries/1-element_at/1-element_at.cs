68e128790e3d57aebdbc81b5440b13e12e61d23e
aydentownsley <aydentownsley@gmail.com>
2021-07-14 11:48:44 -0500

    adding problem 2, and fixing edge case in problem 1 (length and index start at diff points therefore it needs to be if index is >= lenght)

--- 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -4,7 +4,7 @@ class Array
 {
     public static int elementAt(int[] array, int index)
     {
-        if (array.Length < index || index < 0)
+        if (array.Length <= index || index < 0)
         {
             Console.WriteLine("Index out of range");
             return -1;
