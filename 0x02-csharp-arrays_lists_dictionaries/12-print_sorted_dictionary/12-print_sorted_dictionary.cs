5b1f136bdcd1b05d501be9b6de49a436e1f33c44
aydentownsley <aydentownsley@gmail.com>
2021-07-14 15:00:22 -0500

    adding problem 12

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        List<string> keys = new List<string>();
+        keys = myDict.Keys.ToList();
+        keys.Sort();
+        foreach (string key in keys)
+        {
+            Console.WriteLine("{0}: {1}", key, myDict[key]);
+        }
+    }
+}
