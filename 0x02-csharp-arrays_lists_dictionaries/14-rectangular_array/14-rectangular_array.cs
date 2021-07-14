1cf1304f7905b8dfee659f619ebd474a27a28feb
aydentownsley <aydentownsley@gmail.com>
2021-07-14 16:03:48 -0500

    adding problem 14

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] array = new int[5, 5];
+
+        foreach (int x in array)
+        {
+            array[x, x] = 0;
+        }
+        array[2, 2] = 1;
+    
+        int y = 1;
+        foreach (int x in array)
+        {
+            if (y % 5 == 0)
+                Console.WriteLine(x);
+            else
+                Console.Write(x + " ");
+            y++;
+        }
+    }
+}
