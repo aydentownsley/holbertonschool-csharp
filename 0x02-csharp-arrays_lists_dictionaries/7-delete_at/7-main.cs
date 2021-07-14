4ce8ab872aa508887a34b3bdeeea7a2093e21b0b
aydentownsley <aydentownsley@gmail.com>
2021-07-14 13:22:07 -0500

    repushing: local and checker output are different

--- 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-main.cs
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-main.cs
@@ -5,14 +5,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        List<int> myList = new List<int>() {1, 2, 3, 4, 5};
+        List<int> myList = new List<int>() {98, 42, 972, -1, 1024};
 
         foreach (int i in myList)
             Console.WriteLine(i);
 
         Console.WriteLine("----------");
 
-        List.DeleteAt(myList, 2);
+        List.DeleteAt(myList, -1);
 
         foreach (int i in myList)
             Console.WriteLine(i);
