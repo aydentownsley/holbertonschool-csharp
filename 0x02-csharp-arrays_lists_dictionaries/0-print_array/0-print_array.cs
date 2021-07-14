dfa358f8b79a58c85cf9bdda8c67e254f7f5163f
aydentownsley <aydentownsley@gmail.com>
2021-07-14 10:38:14 -0500

    adding readme and problem 0

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+           Console.WriteLine("Size cannot be negative");
+           return null;
+        }
+
+        int x;
+        int[] arr = new int[size];
+        if (size == 0)
+        {
+            Console.WriteLine();
+            return arr;
+        }
+
+        for (x = 0; x < size; x++)
+        {
+            arr[x] = x;
+            if (x == size - 1)
+                Console.Write(arr[x]);
+            else
+                Console.Write(arr[x] + " ");
+        }
+
+        Console.WriteLine();
+        return arr;
+    }
+}
