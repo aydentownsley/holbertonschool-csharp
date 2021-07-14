19d0ef9f155d6d4bc9a91bf5b9df0fe0ca9ebdde
aydentownsley <aydentownsley@gmail.com>
2021-07-14 15:37:31 -0500

    adding edge case for None

--- 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -6,6 +6,8 @@ class Dictionary
 {
     public static string BestScore(Dictionary<string, int> myList)
     {
+        if (myList.Count == 0)
+            return "None";
         string name = "";
         int score = myList.Values.Max();
         foreach (var key in myList)
