134ee34cec7b24ff922aca5bdedfc2e835ab55eb
aydentownsley <aydentownsley@gmail.com>
2021-07-15 16:42:13 -0500

    adding problem 10

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        int count = 0;
+        if (myLList.Count == 0)
+            return 0;
+        else if (n > myLList.Count)
+            return 0;
+        foreach (int item in myLList)
+        {
+            if (count == n)
+                return item;
+            count++;
+        }
+        return 0;
+    }
+}
