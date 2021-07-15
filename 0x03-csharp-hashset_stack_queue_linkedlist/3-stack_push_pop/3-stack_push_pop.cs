9d8b3d78ba3cdbec36082de067783ea807b8cc67
aydentownsley <aydentownsley@gmail.com>
2021-07-15 14:39:02 -0500

    added stack is empty check

--- 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -6,7 +6,10 @@ class MyStack
     public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
     {
         Console.WriteLine("Number of items: {0}", aStack.Count);
-        Console.WriteLine("Top item: {0}", aStack.Peek());
+        if (aStack.Count > 0)
+            Console.WriteLine("Top item: {0}", aStack.Peek());
+        else
+            Console.WriteLine("The stack is empty");
         Console.WriteLine("Stack contains \"{0}\": {1}", search, aStack.Contains(search));
         while (aStack.Contains(search))
         {
