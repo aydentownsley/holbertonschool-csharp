560e224abaa9260ebfbe17e5d42a2f25bad059cc
aydentownsley <aydentownsley@gmail.com>
2021-07-15 14:10:11 -0500

    adding problem 2

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        List<int> result = new List<int>();
+        foreach (int item in list1)
+        {
+            if (!list2.Contains(item))
+            {
+                result.Add(item);
+            }
+        }
+        foreach (int item in list2)
+        {
+            if (!list1.Contains(item))
+            {
+                result.Add(item);
+            }
+        }
+        result.Sort();
+        return result;
+    }
+}
