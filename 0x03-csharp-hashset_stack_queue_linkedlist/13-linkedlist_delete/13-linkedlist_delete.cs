2200d4d67f2ec1801988920fb64c6ec42eb45ba0
aydentownsley <aydentownsley@gmail.com>
2021-07-15 17:38:16 -0500

    adding problem 13

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        LinkedListNode<int> current = myLList.First;
+        int count = 0;
+        while (current != null)
+        {
+            if (count == index)
+                myLList.Remove(current);
+            current = current.Next;
+            count++;    
+        }
+    }
+}
