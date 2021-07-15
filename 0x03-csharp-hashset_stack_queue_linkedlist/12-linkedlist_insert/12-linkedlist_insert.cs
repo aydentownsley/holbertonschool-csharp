fbca415eef213ee3f5a14c5b2136c77b0082becf
aydentownsley <aydentownsley@gmail.com>
2021-07-15 17:16:16 -0500

    adding problem 12

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> newNode = new LinkedListNode<int>(n);
+        LinkedListNode<int> current = myLList.First;
+        for (int x = 0; x < myLList.Count; x++)
+        {
+            if (n < current.Next.Value)
+            {
+                myLList.AddAfter(current, newNode);
+                return newNode;
+            }
+            else
+                current = current.Next;
+        }
+        return newNode;
+    }
+}
