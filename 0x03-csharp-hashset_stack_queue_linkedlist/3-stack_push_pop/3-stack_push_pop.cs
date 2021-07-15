18177b093eb95861f6026525345aa46113aa08c3
aydentownsley <aydentownsley@gmail.com>
2021-07-15 14:34:41 -0500

    removing a pop

--- 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -8,10 +8,8 @@ class MyStack
         Console.WriteLine("Number of itmes: {0}", aStack.Count);
         Console.WriteLine("Top item: {0}", aStack.Peek());
         Console.WriteLine("Stack contains \"{0}\": {1}", search, aStack.Contains(search));
-        if (aStack.Contains(search))
+        while (aStack.Contains(search))
         {
-            while (aStack.Peek() != search)
-                aStack.Pop();
             aStack.Pop();
         }
         aStack.Push(newItem);
