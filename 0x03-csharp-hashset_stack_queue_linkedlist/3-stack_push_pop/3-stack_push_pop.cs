d751f0c675d023c0730cb3f60bbc370bc7e6b3c9
aydentownsley <aydentownsley@gmail.com>
2021-07-15 14:36:20 -0500

    typo

--- 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -5,7 +5,7 @@ class MyStack
 {
     public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
     {
-        Console.WriteLine("Number of itmes: {0}", aStack.Count);
+        Console.WriteLine("Number of items: {0}", aStack.Count);
         Console.WriteLine("Top item: {0}", aStack.Peek());
         Console.WriteLine("Stack contains \"{0}\": {1}", search, aStack.Contains(search));
         while (aStack.Contains(search))
