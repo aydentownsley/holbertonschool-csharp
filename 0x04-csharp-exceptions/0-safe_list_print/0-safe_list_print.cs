11b1e2da63aa92490673d03a2b7142f02b2e9f32
aydentownsley <aydentownsley@gmail.com>
2021-07-26 11:06:36 -0500

    adding project 0x04

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int count = 0;
+        try
+        {
+            foreach (int element in myList)
+            {
+                Console.WriteLine(element);
+                count++;
+            }
+            return count;
+        }
+        catch (System.IndexOutOfRangeException)
+        {
+            return count;
+        }
+    }
+}
