16dc9f8bbd7849230efabd369e0530848785d1fd
aydentownsley <aydentownsley@gmail.com>
2021-08-20 09:15:53 -0500

    adding problem 9

---
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class VectorMath
+{
+    /// <summary> Muliplies a vector by a scalar. </summary>
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        if (vector == null)
+            return (new double[] {-1, -1});
+        if (vector.Length < 2 || vector.Length > 3)
+            return (new double[] {-1, -1});
+
+        double[] result = new double[vector.Length];
+        for (int i = 0; i < vector.Length; i++)
+        {
+            result[i] = vector[i] * scalar;
+        }
+        return result;
+    }
+}
