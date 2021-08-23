59126fcab225645d9843c4e59bd76433aa1819cb
aydentownsley <aydentownsley@gmail.com>
2021-08-23 18:12:01 -0500

    adding 16

---
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+    {
+        if (matrix == null)
+            return (new double[,] {{-1}});
+        if (matrix.GetLength(0) != matrix.GetLength(1))
+            return (new double[,] {{-1}});
+        if (matrix.GetLength(0) < 2 || matrix.GetLength(0) > 3)
+            return (new double[,] {{-1}});
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+            for (int j = 0; j < matrix.GetLength(1); j++)
+                matrix[i, j] *= scalar;
+
+        return matrix;
+    }
+}
