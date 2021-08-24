2e1a1fac6f3af8860d4240e5f71352b13b336abb
aydentownsley <aydentownsley@gmail.com>
2021-08-24 17:34:15 -0500

    adding 30

---
+++ 0x09-csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[] CrossProduct(double[] vector1, double[] vector2)
+    {
+        if (vector1.Length != 3 || vector2.Length != 3)
+            return new double[] {-1};
+
+        double[] result = new double[3];
+
+        for (int i = 0; i < 3; i++)
+        {
+            result[i] = vector1[i] * vector2[(i + 1) % 3] - vector1[(i + 1) % 3] * vector2[i];
+        }
+
+        return result;
+    }
+}
