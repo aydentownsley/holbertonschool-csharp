713969efe6eed6ae1a4719a0d494dd015accca45
aydentownsley <aydentownsley@gmail.com>
2021-08-24 15:23:46 -0500

    added 24

---
+++ 0x09-csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] Shear2D(double[,] matrix, char direction, double factor)
+    {
+        if (matrix == null || matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+            return (new double[,] {{-1}});
+
+        if (direction == 'x')
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                matrix[i, 0] = matrix[i, 0] + factor * matrix[i, 1];
+            }
+            return matrix;
+        }
+        else if (direction == 'y')
+        {
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                matrix[0, i] = matrix[0, i] + factor * matrix[1, i];
+            }
+            return matrix;
+        }
+        else
+            return (new double[,] {{-1}});
+    }
+}
