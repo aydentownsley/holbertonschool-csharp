dc5a9dce54e12ceceaa2ca4df67d86beab6c7651
aydentownsley <aydentownsley@gmail.com>
2021-08-24 12:35:42 -0500

    fixing round

--- 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -8,12 +8,12 @@ class MatrixMath
         if (matrix == null || matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
             return (new double[,] {{-1}});
 
-        double result = new double[matrix.GetLength(0), matrix.GetLength(1)];
+        double result = new double[2, 2];
 
-        result[0, 0] = matrix[0,0] * Math.Cos(180/Math.PI * angle) - matrix[0,1] * Math.Sin(180/Math.PI * angle);
-        result[0, 1] = matrix[0,0] * Math.Sin(180/Math.PI * angle) + matrix[0,1] * Math.Cos(180/Math.PI * angle);
-        result[1, 0] = matrix[1,0] * Math.Cos(180/Math.PI * angle) - matrix[1,1] * Math.Sin(180/Math.PI * angle);
-        result[1, 1] = matrix[1,0] * Math.Sin(180/Math.PI * angle) + matrix[1,1] * Math.Cos(180/Math.PI * angle);
+        result[0, 0] = matrix[0,0] * Math.Cos(180/Math.PI * angle) - matrix[0,1] * Math.Sin(180/Math.PI * angle).Round(0.00);
+        result[0, 1] = matrix[0,0] * Math.Sin(180/Math.PI * angle) + matrix[0,1] * Math.Cos(180/Math.PI * angle).Round(0.00);
+        result[1, 0] = matrix[1,0] * Math.Cos(180/Math.PI * angle) - matrix[1,1] * Math.Sin(180/Math.PI * angle).Round(0.00);
+        result[1, 1] = matrix[1,0] * Math.Sin(180/Math.PI * angle) + matrix[1,1] * Math.Cos(180/Math.PI * angle).Round(0.00);
 
         return result;
     }
