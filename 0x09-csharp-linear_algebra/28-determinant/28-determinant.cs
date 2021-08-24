046bb46bfcb637337b30c3b3aeb208c7047344fa
aydentownsley <aydentownsley@gmail.com>
2021-08-24 16:22:29 -0500

    typo /

--- 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -2,7 +2,7 @@
 
 class MatrixMath
 {
-    public static double Determinant(double[,] matrix)\
+    public static double Determinant(double[,] matrix)
     {
         if (matrix.GetLength(0) != matrix.GetLength(1))
             return -1;
@@ -13,5 +13,6 @@ class MatrixMath
             return matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
         if (matrix.GetLength(0) == 3)
             return matrix[0, 0] * matrix[1, 1] * matrix[2, 2] + matrix[0, 1] * matrix[1, 2] * matrix[2, 0] + matrix[0, 2] * matrix[1, 0] * matrix[2, 1] - matrix[0, 2] * matrix[1, 1] * matrix[2, 0] - matrix[0, 1] * matrix[1, 0] * matrix[2, 2] - matrix[0, 0] * matrix[1, 2] * matrix[2, 1];
-    }
+        return -1;
+   }
 }
