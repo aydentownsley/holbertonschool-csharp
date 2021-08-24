e17b6984048052366f943053474dc3e9c7ed3eed
aydentownsley <aydentownsley@gmail.com>
2021-08-24 15:52:12 -0500

    adding 28

---
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double Determinant(double[,] matrix)\
+    {
+        if (matrix.GetLength(0) != matrix.GetLength(1))
+            return -1;
+        if (matrix.GetLength(0) < 2 || matrix.GetLength(0) > 3)
+            return -1;
+
+        if (matrix.GetLength(0) == 2)
+            return matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
+        if (matrix.GetLength(0) == 3)
+            return matrix[0, 0] * matrix[1, 1] * matrix[2, 2] + matrix[0, 1] * matrix[1, 2] * matrix[2, 0] + matrix[0, 2] * matrix[1, 0] * matrix[2, 1] - matrix[0, 2] * matrix[1, 1] * matrix[2, 0] - matrix[0, 1] * matrix[1, 0] * matrix[2, 2] - matrix[0, 0] * matrix[1, 2] * matrix[2, 1];
+    }
+}
