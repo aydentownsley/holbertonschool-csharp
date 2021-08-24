1d39b3228cee89570a1f6d3fd0a42b8326ba6033
aydentownsley <aydentownsley@gmail.com>
2021-08-23 19:10:24 -0500

    adding 18

---
+++ 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
+    {
+        if (matrix1 == null || matrix2 == null)
+            return (new double[,] {{-1}});
+
+        if (matrix1.GetLength(1) != matrix2.GetLength(1))
+            return (new double[,] {{-1}});
+
+        double[,] result = new double[matrix1.GetLength(0), matrix2.GetLength(1)];
+
+        for (int x = 0; x < result.GetLength(0); x++)
+        {
+            for (int y = 0; y < result.GetLength(1); y++)
+            {
+                result[x, y] = 0;
+                for (int i = 0; i < matrix1.GetLength(1); i++)
+                {
+                    result[x, y] += matrix1[x, i] * matrix2[i, y];
+                }
+            }
+        }
+        return result;
+    }
+}
