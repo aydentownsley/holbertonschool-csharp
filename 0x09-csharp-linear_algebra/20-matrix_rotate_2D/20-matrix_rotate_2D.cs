c480d301bbdd65e82b14d91e7cfe85f880a46e93
aydentownsley <aydentownsley@gmail.com>
2021-08-24 13:24:36 -0500

    fixing round issue

--- 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -9,10 +9,10 @@ class MatrixMath
             return (new double[,] {{-1}});
 
         double[,] result = new double[2, 2];
-        result[0, 0] = Math.Round(matrix[0,0] * Math.Cos(angle) - matrix[0,1] * Math.Sin(angle), 0.00);
-        result[0, 1] = Math.Round(matrix[0,0] * Math.Sin(angle) + matrix[0,1] * Math.Cos(angle), 0.00);
-        result[1, 0] = Math.Round(matrix[1,0] * Math.Cos(angle) - matrix[1,1] * Math.Sin(angle), 0.00);
-        result[1, 1] = Math.Round(matrix[1,0] * Math.Sin(angle) + matrix[1,1] * Math.Cos(angle), 0.00);
+        result[0, 0] = Math.Round(matrix[0,0] * Math.Cos(angle) - matrix[0,1] * Math.Sin(angle), 2);
+        result[0, 1] = Math.Round(matrix[0,0] * Math.Sin(angle) + matrix[0,1] * Math.Cos(angle), 2);
+        result[1, 0] = Math.Round(matrix[1,0] * Math.Cos(angle) - matrix[1,1] * Math.Sin(angle), 2);
+        result[1, 1] = Math.Round(matrix[1,0] * Math.Sin(angle) + matrix[1,1] * Math.Cos(angle), 2);
 
         return result;
     }
