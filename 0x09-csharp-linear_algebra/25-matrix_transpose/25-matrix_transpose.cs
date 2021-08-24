301e9ce9e3eb5adf33aa296a23b506b55b8a8986
aydentownsley <aydentownsley@gmail.com>
2021-08-24 15:40:18 -0500

    adding 25

---
+++ 0x09-csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] Transpose(double[,] matrix)
+    {
+        if (matrix == null || matrix.GetLength(0) == 0 || matrix.GetLength(1) == 0)
+            return (new double[,] {});
+
+        double[,] transposed = new double[matrix.GetLength(1), matrix.GetLength(0)];
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                transposed[j, i] = matrix[i, j];
+            }
+        }
+
+        return transposed;
+    }
+}
