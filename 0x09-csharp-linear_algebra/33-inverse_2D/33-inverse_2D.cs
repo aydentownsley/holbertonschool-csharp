6565dcf65a5ddcd97688eb2c135c791eb8c12c4d
aydentownsley <aydentownsley@gmail.com>
2021-08-25 16:13:22 -0500

    adding 33

--- 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
+++ 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -11,10 +11,10 @@ class MatrixMath
 
         double det = matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
 
-        inverse[0, 0] = matrix[1, 1] / det;
-        inverse[0, 1] = -matrix[0, 1] / det;
-        inverse[1, 0] = -matrix[1, 0] / det;
-        inverse[1, 1] = matrix[0, 0] / det;
+        inverse[0, 0] = Math.Round(matrix[1, 1] / det, 2);
+        inverse[0, 1] = Math.Round(-matrix[0, 1] / det, 2);
+        inverse[1, 0] = Math.Round(-matrix[1, 0] / det, 2);
+        inverse[1, 1] = Math.Round(matrix[0, 0] / det, 2);
 
         return inverse;
     }
