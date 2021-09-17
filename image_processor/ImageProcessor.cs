3000ca411cc7bc5eaac75e40c2c50f7ea8ca9b21
Ayden Townsley <aydentownsley@gmail.com>
2021-09-17 11:26:07 -0500

    adding grayscale

--- image_processor/ImageProcessor.cs
+++ image_processor/ImageProcessor.cs
@@ -22,4 +22,25 @@ class ImageProcessor
             bitmap.Save(new_filename);
         }
     }
+
+    public static void Grayscale(string[] filenames)
+    {
+        foreach (string filename in filenames)
+        {
+            Bitmap bitmap = new Bitmap(filename);
+            for (int x = 0; x < bitmap.Width; x++)
+            {
+                for (int y = 0; y < bitmap.Height; y++)
+                {
+                    Color color = bitmap.GetPixel(x, y);
+                    int grayScale = (int)((color.R * 0.3) + (color.G * 0.59) + (color.B * 0.11));
+                    color = Color.FromArgb(255, grayScale, grayScale, grayScale);
+                    bitmap.SetPixel(x, y, color);
+                }
+            }
+            string new_filename = Path.GetFileNameWithoutExtension(filename) + "_grayscale" + Path.GetExtension(filename);
+            bitmap.Save(new_filename);
+        }
+    }
+
 }
