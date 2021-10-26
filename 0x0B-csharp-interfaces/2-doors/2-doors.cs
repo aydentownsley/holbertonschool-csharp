982b08640575e95efba3dc27b5fb8f68d60d7686
Ayden Townsley <aydentownsley@gmail.com>
2021-10-26 13:37:37 -0500

    adding 2

---
+++ 0x0B-csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+abstract class Base
+{
+    public string name { get; set; }
+    public override string ToString()
+    {
+        return name + " is a " + GetType();
+    }
+}
+
+/// <summary>
+/// Interface for Intraction
+/// </summary>
+interface IInteractive
+{
+  void Interact();
+}
+
+/// <summary>
+/// Interface for breakable
+/// </summary>
+interface IBreakable
+{
+  int durability { get; set; }
+  void Break();
+}
+
+/// <summary>
+/// Interface for Collectable
+/// </summary>
+interface ICollectable
+{
+  bool isCollected { get; set; }
+  void Collect();
+}
+
+class Door : Base, IInteractive
+{
+  public Door(string name = "Door")
+  {
+    this.name = name;
+  }
+  public void Interact()
+  {
+    Console.WriteLine("You try to open the {0}. It's locked.", name);
+  }
+}
