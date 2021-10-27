2e2b0b2d3c57d43d811ac4ed7110877f835ef2d5
Ayden Townsley <aydentownsley@gmail.com>
2021-10-27 15:52:17 -0500

    setting up project and adding problem 0

---
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+/// <summary> Player Class with stats </summary>
+public class Player
+{
+    private string name;
+    private float maxHP;
+    private float hp;
+
+    /// <summary> Player Constructor to set stats </summary>
+    public Player(string name = "Player", float maxHP = 100f)
+    {
+        this.name = name;
+        if (maxHP <= 0f)
+        {
+            this.maxHP = this.hp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        else
+            this.maxHP = this.hp = maxHP;
+    }
+
+    /// <summary> Prints health of Player </summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine(this.name + " has " + this.hp + " / " + this.maxHP + " health");
+    }
+}
---
+++ 0x0C-csharp-delegates_events/README.md
@@ -0,0 +1,3 @@
+# 0x0C. C# - Delegates, Events
+
+## Delegates allow code bases to be extended while mainting core functionality.
