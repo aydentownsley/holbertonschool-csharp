89c0049abc9369f8b7f1ccc7af4b8528b9eac24d
Ayden Townsley <aydentownsley@gmail.com>
2021-10-28 13:52:51 -0500

    reworked Eventhandler

---
+++ 0x0C-csharp-delegates_events/4-check_yourself/4-check_yourself.cs
@@ -0,0 +1,128 @@
+﻿using System;
+
+/// <summary> Handles health </summary>
+public class CurrentHPArgs : EventArgs
+{
+    /// <summary> currentHp check </summary>
+    public readonly float currentHp;
+
+    /// <summary> sets newHP to current </summary>
+    public CurrentHPArgs(float newHP)
+    {
+        newHP = this.currentHp;
+    }
+}
+
+/// <summary> enum for modifiers </summary>
+public enum Modifier
+{
+    /// <summary> Weak Mod </summary>
+    Weak,
+    /// <summary> Base Mod </summary>
+    Base,
+    /// <summary> Strong Mod </summary>
+    Strong
+}
+
+/// <summary> Delegate to calculate modifier </summary>
+public delegate float CalculateModifier(float baseValue, Modifier modifier);
+
+/// <summary> Delegate to calculate health for damage and healing </summary>
+public delegate float CalculateHealth(float points);
+
+/// <summary> Player Class with stats </summary>
+public class Player
+{
+    private string name;
+    private float maxHP;
+    private float hp;
+    private string status;
+    /// <summary> Event handler for health </summary>
+    public EventHandler<CurrentHPArgs> HPCheck;
+
+    /// <summary> Player Constructor to set stats </summary>
+    public Player(string name = "Player", float maxHP = 100f)
+    {
+        this.status = this.name + " is ready to go!";
+        this.name = name;
+        if (maxHP <= 0f)
+        {
+            this.maxHP = this.hp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        else
+            this.maxHP = this.hp = maxHP;
+
+        this.HPCheck += CheckStatus;
+    }
+
+    /// <summary> Prints health of Player </summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine(this.name + " has " + this.hp + " / " + this.maxHP + " health");
+    }
+
+    /// <summary> decreases health of player </summary>
+    public void TakeDamage(float damage)
+    {
+        if (damage <= 0)
+            Console.WriteLine(this.name + " takes 0 damage!");
+        else
+        {
+            ValidateHP(this.hp - damage);
+            Console.WriteLine(this.name + " takes " + damage + " damage!");
+        }
+    }
+
+    /// <summary> increases health of player </summary>
+    public void HealDamage(float heal)
+    {
+        if (heal <= 0)
+            Console.WriteLine(this.name + " heals 0 HP!");
+        else
+        {
+            ValidateHP(this.hp + heal);
+            Console.WriteLine(this.name + " heals " + heal + " HP!");
+        }
+    }
+
+    /// <summary> validates calculated health is within bounds of maxHP </summary>
+    public void ValidateHP(float newHP)
+    {
+        if (newHP <= 0)
+            this.hp = 0;
+        else if (newHP >= maxHP)
+            this.hp = maxHP;
+        else
+            this.hp = newHP;
+
+        HPCheck(this, new CurrentHPArgs(this.hp));
+    }
+
+    /// <summary> apply modifer to value entered </summary>
+    public float ApplyModifier(float baseValue, Modifier modifier)
+    {
+        if (modifier.ToString() == "Weak")
+            return (baseValue * 0.5f);
+        else if (modifier.ToString() == "Base")
+            return (baseValue);
+        else if (modifier.ToString() == "Strong")
+            return (baseValue * 1.5f);
+        else
+            return (baseValue);
+    }
+
+    private void CheckStatus(object sender, CurrentHPArgs e)
+    {
+        if (e.currentHp == maxHP)
+            Console.WriteLine(this.name + " is in perfect health!");
+        else if (e.currentHp >= (0.5f * maxHP) && e.currentHp < maxHP)
+            Console.WriteLine(this.name + " is doing well!");
+        else if (e.currentHp >= (0.25f * maxHP) && e.currentHp < (0.5f * maxHP))
+            Console.WriteLine(this.name + " isn't doing too great...");
+        else if (e.currentHp > 0 && e.currentHp < (0.25f * maxHP))
+            Console.WriteLine(this.name + " needs help!");
+        else
+            Console.WriteLine(this.name + " is knocked out!");
+    }
+}
